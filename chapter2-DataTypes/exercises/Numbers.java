770a395188aa7a348e89b64186903b44b4d85c73
Patrick Harris <patricknpharris@gmail.com>
2018-10-18 12:18:00 +0100

    basic string declaration completed

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,7 +1,8 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int integer
+    integer = 1
     // declare a double variable with a value
 
     // store the result of adding the two together in a variable
--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String patrickString = "Patrick";
     // declare the second name
-
+    String victorString = "Victor";
     // create your message
-
+    String messageString = patrickString + " sits next to " + victorString;
     // print your message!
-
+    System.out.println(messageString);
   }
 }
