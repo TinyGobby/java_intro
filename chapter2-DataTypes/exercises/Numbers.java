fea1b9a7287b94f51bd5a86753723411e925bbf4
Patrick Harris <patricknpharris@gmail.com>
2018-10-18 12:21:26 +0100

    basic number implementation added

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,13 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-    int integer
-    integer = 1
+    int integerVariable = 1;
     // declare a double variable with a value
-
+    double doubleVariable = 1.1;
     // store the result of adding the two together in a variable
-
+    double added = doubleVariable + integerVariable;
     // print it
-
+    System.out.println(added);
   }
 }
