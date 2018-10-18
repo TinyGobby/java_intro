9be7cbb0e6aee4567f2e460ca47561d0435b2f4d
Patrick Harris <patricknpharris@gmail.com>
2018-10-18 14:50:55 +0100

    enhanced for loop version

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -5,8 +5,15 @@ public class LoopThroughArray {
   public static void main(String[] args) {
     // loop through an integer array and print out each element
     Integer arr[] = { 1, 2, 3, 4 };
+
+    // simple for loop
     for(int i=0; i < arr.length; i++){
-      System.out.println(arr[i]);
+      System.out.println("for: " + arr[i]);
+    }
+
+    // enhanced for loop
+    for(int number: arr){
+      System.out.println("enhanced: " + number);
     }
   }
 }
