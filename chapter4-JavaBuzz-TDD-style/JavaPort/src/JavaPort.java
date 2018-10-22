7a3439b929c1d3845d729b62ac54fdab6a1dc09f
Patrick Harris <patricknpharris@gmail.com>
2018-10-22 17:20:43 +0100

    basic working airport built

--- chapter4-JavaBuzz-TDD-style/JavaPort/src/JavaPort.java
+++ chapter4-JavaBuzz-TDD-style/JavaPort/src/JavaPort.java
@@ -1,2 +1,9 @@
 public class JavaPort {
+    public String land(String plane) {
+        return "Landed";
+    }
+
+    public String takeoff(String plane) {
+        return "Taken off";
+    }
 }
--- chapter4-JavaBuzz-TDD-style/JavaPort/src/JavaPortTest.java
+++ chapter4-JavaBuzz-TDD-style/JavaPort/src/JavaPortTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaPortTest {
+    private String plane;
+    private JavaPort airport;
+
+
+    @BeforeEach
+    public void initJavaPort() {
+        airport = new JavaPort();
+        plane = "plane";
+    }
+
+    @Test
+    public void testLanded() {
+        String output = airport.land(plane);
+        assertEquals("Landed", output);
+    }
+
+    @Test
+    public void testTakeoff() {
+        String output = airport.takeoff(plane);
+        assertEquals("Taken off", output);
+    }
 }
